fff121fdc80ca050ead0f5c6ba90a7148d6023ad
Jens de Ruiter <jensgamesnl@gmail.com>
2025-03-04 17:08:38 +0100

    Make tests assert the library's parsed results

--- tests/test_live.py
+++ tests/test_live.py
@@ -1,7 +1,9 @@
 import unittest
 from datetime import datetime, timedelta
 
-from marktplaats import SearchQuery, SortBy, SortOrder, Condition, category_from_name
+from marktplaats import SearchQuery, SortBy, SortOrder, Condition, category_from_name, PriceType, ListingLocation, \
+    ListingSeller
+from marktplaats.models.listing_seller import Seller
 
 
 class LiveSearchQueryTest(unittest.TestCase):
@@ -9,6 +11,55 @@ class LiveSearchQueryTest(unittest.TestCase):
     Live tests (sent to Marktplaats) to ensure they
     haven't deployed breaking changes.
     """
+
+    def _validate_response(self, search):
+        listings = search.get_listings()
+
+        for listing in listings:
+            self.assertIsInstance(listing.title, str)
+            self.assertIsInstance(listing.description, str)
+            self.assertIsInstance(listing.price, float)
+            self.assertIsInstance(listing.price_as_string(lang="nl"), str)
+            self.assertIsInstance(listing.price_as_string(lang="en"), str)
+            self.assertIsInstance(listing.price_type, PriceType)
+
+            self.assertIsInstance(listing.link, str)
+            self.assertTrue(listing.link.startswith("https://"))
+
+            # the location object
+            self.assertIsInstance(listing.location, ListingLocation)
+            self.assertIsInstance(listing.location.city, str)
+            self.assertIsInstance(listing.location.latitude, float)
+            self.assertIsInstance(listing.location.longitude, float)
+
+            # the seller object
+            self.assertIsInstance(listing.seller, ListingSeller)
+            self.assertIsInstance(listing.seller.id, int)
+            self.assertIsInstance(listing.seller.name, str)
+            self.assertIsInstance(listing.seller.is_verified, bool)
+
+            # the datetime object
+            self.assertIsInstance(listing.date, datetime)
+            # should be greater than what we queried for
+            self.assertGreater(listing.date, datetime.now() - timedelta(days=7))
+
+            # the full seller object (another request)
+            seller = listing.seller.get_seller()
+            self.assertIsInstance(seller, Seller)
+            self.assertIsInstance(seller.id, int)
+            self.assertIsInstance(seller.name, str)
+            self.assertIsInstance(seller.is_verified, bool)
+            self.assertIsNotNone(seller.average_score)
+            self.assertIsInstance(seller.number_of_reviews, int)
+            self.assertIsInstance(seller.bank_account, bool)
+            self.assertIsInstance(seller.identification, bool)
+            self.assertIsInstance(seller.phone_number, bool)
+
+            for image in listing.images:
+                self.assertIsInstance(image.extra_small, str)
+                self.assertIsInstance(image.medium, str)
+                self.assertIsInstance(image.large, str)
+                self.assertIsInstance(image.extra_large, str)
 
     def test_request(self):
         search = SearchQuery("fiets",
@@ -23,33 +74,8 @@ class LiveSearchQueryTest(unittest.TestCase):
                              offered_since=datetime.now() - timedelta(days=7),
                              category=category_from_name("Fietsen en Brommers"))
 
-        listings = search.get_listings()
+        self._validate_response(search)
 
-        for listing in listings:
-            print(listing.title)
-            print(listing.description)
-            print(listing.price)
-            print(listing.price_as_string(lang="nl"))
-            print(listing.price_type)
-            print(listing.link)
-
-            # the location object
-            print(listing.location)
-
-            # the seller object
-            print(listing.seller)
-
-            # the datetime object
-            print(listing.date)
-
-
-            # the full seller object (another request)
-            print(listing.seller.get_seller())
-
-            for image in listing.images:
-                print(image.medium)
-
-            print("-----------------------------")
 
     def test_request_with_condition(self):
         search = SearchQuery("schijf",
@@ -60,33 +86,7 @@ class LiveSearchQueryTest(unittest.TestCase):
                              condition=Condition.NOT_WORKING,
                              category=category_from_name("Computers en Software"))
 
-        listings = search.get_listings()
-
-        for listing in listings:
-            print(listing.title)
-            print(listing.description)
-            print(listing.price)
-            print(listing.price_as_string(lang="nl"))
-            print(listing.price_type)
-            print(listing.link)
-
-            # the location object
-            print(listing.location)
-
-            # the seller object
-            print(listing.seller)
-
-            # the datetime object
-            print(listing.date)
-
-
-            # the full seller object (another request)
-            print(listing.seller.get_seller())
-
-            for image in listing.images:
-                print(image.medium)
-
-            print("-----------------------------")
+        self._validate_response(search)
 
 
 if __name__ == '__main__':
