7c4ff4b58d9e5a835801c1038b96c71b4aff0874
Jens de Ruiter <jensgamesnl@gmail.com>
2025-05-21 11:28:50 +0200

    Fix image path in test

--- tests/test_images.py
+++ tests/test_images.py
@@ -12,7 +12,8 @@ class ImageFetchTest(unittest.TestCase):
     """
 
     def test_parse_images(self):
-        with open('mock/image_response.html', 'r') as file:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(os.path.join(dir_path, 'mock/image_response.html'), 'r') as file:
             mock_response = file.read()
         with requests_mock.Mocker() as m:
             m.get(
