5a2c761df78aa9e0aa408bf9fc7a3ead13b49995
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-05-28 14:18:50 +0200

    Type __init__.py

--- marktplaats/models/__init__.py
+++ marktplaats/models/__init__.py
@@ -2,3 +2,4 @@ from .listing_image import ListingFirstImage as ListingFirstImage
 from .listing_seller import ListingSeller as ListingSeller
 from .listing_location import ListingLocation as ListingLocation
 from .listing import Listing as Listing
+from .price_type import PriceType as PriceType
