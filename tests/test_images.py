afd5289b062455eda7e2c367cd9199796f693605
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-05-29 10:05:44 +0200

    Format

--- tests/test_images.py
+++ tests/test_images.py
@@ -13,7 +13,9 @@ class TestImageFetch:
 
     def test_parse_images(self) -> None:
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        with open(os.path.join(dir_path, "mock/image_response.html"), encoding="utf-8") as file:
+        with open(
+            os.path.join(dir_path, "mock/image_response.html"), encoding="utf-8"
+        ) as file:
             mock_response = file.read()
         with requests_mock.Mocker() as m:
             m.get(
