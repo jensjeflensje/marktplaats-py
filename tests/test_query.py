3e85952c0c647bf5d4d6110c2eab183d07999bb6
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-05-30 11:57:36 +0200

    Replace unittest.mock with requests_mock

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,9 +1,6 @@
 from __future__ import annotations
 
 from datetime import date, datetime
-from unittest.mock import (
-    patch,  # TODO(GideonBear): switch to something else?  # noqa: FIX002, TD003
-)
 
 import pytest
 import requests
@@ -12,93 +9,25 @@ import requests_mock
 from marktplaats import BadStatusCodeError, JSONDecodeError, PriceType, SearchQuery
 from marktplaats.categories import category_from_name
 from marktplaats.models import ListingLocation
+from tests.utils import get_mock_file
 
 
 """Basic tests to test search query functionality."""
 
 
 def test_request() -> None:
-    with patch("requests.get") as get_request:
-        get_request.return_value.status_code = 200
-        get_request.return_value.json.return_value = {
-            "totalResultCount": 100,
-            "listings": [
-                {
-                    "itemId": "m2064554806",
-                    "title": "Batavus damesfiets 26 inch",
-                    "description": "Degelijke batavus damesfiets 26 inch met slot, verlichting en versnellingen.",
-                    "categorySpecificDescription": "Degelijke batavus damesfiets 26 inch met slot, verlichting en versnellingen.",
-                    "thinContent": True,
-                    "priceInfo": {"priceCents": 7500, "priceType": "FIXED"},
-                    "location": {
-                        "cityName": "Nieuwerkerk aan den IJssel",
-                        "countryName": "Nederland",
-                        "countryAbbreviation": "NL",
-                        "distanceMeters": 1000,
-                        "isBuyerLocation": False,
-                        "onCountryLevel": False,
-                        "abroad": False,
-                        "latitude": 51.965397128056,
-                        "longitude": 4.6119871732025,
-                    },
-                    "date": "10 mrt 24",
-                    "imageUrls": [
-                        "//images.marktplaats.com/api/v1/listing-mp-p/images/63/636424bb-b0bd-458b-964c-747af344c793?rule=ecg_mp_eps$_82.jpg"
-                    ],
-                    "sellerInformation": {
-                        "sellerId": 7405065,
-                        "sellerName": "Vogel",
-                        "showSoiUrl": True,
-                        "showWebsiteUrl": False,
-                        "isVerified": False,
-                    },
-                    "categoryId": 447,
-                    "priorityProduct": "NONE",
-                    "videoOnVip": False,
-                    "urgencyFeatureActive": False,
-                    "napAvailable": False,
-                    "attributes": [
-                        {
-                            "key": "condition",
-                            "value": "Gebruikt",
-                            "values": ["Gebruikt"],
-                        },
-                        {
-                            "key": "delivery",
-                            "value": "Ophalen",
-                            "values": ["Ophalen"],
-                        },
-                    ],
-                    "extendedAttributes": [
-                        {
-                            "key": "condition",
-                            "value": "Gebruikt",
-                            "values": ["Gebruikt"],
-                        },
-                        {
-                            "key": "delivery",
-                            "value": "Ophalen",
-                            "values": ["Ophalen"],
-                        },
-                    ],
-                    "traits": ["PACKAGE_FREE"],
-                    "verticals": ["bicycles_and_mopeds", "bicycles_ladies_bike"],
-                    "pictures": [
-                        {
-                            "id": 9322832634,
-                            "mediaId": "",
-                            "url": "https://images.marktplaats.com/api/v1/listing-mp-p/images/63/636424bb-b0bd-458b-964c-747af344c793?rule=ecg_mp_eps$_#.jpg",
-                            "extraSmallUrl": "https://images.marktplaats.com/api/v1/listing-mp-p/images/63/636424bb-b0bd-458b-964c-747af344c793?rule=ecg_mp_eps$_14.jpg",
-                            "mediumUrl": "https://images.marktplaats.com/api/v1/listing-mp-p/images/63/636424bb-b0bd-458b-964c-747af344c793?rule=ecg_mp_eps$_82.jpg",
-                            "largeUrl": "https://images.marktplaats.com/api/v1/listing-mp-p/images/63/636424bb-b0bd-458b-964c-747af344c793?rule=ecg_mp_eps$_83.jpg",
-                            "extraExtraLargeUrl": "https://images.marktplaats.com/api/v1/listing-mp-p/images/63/636424bb-b0bd-458b-964c-747af344c793?rule=ecg_mp_eps$_85.jpg",
-                            "aspectRatio": {"width": 3, "height": 4},
-                        }
-                    ],
-                    "vipUrl": "/v/fietsen-en-brommers/fietsen-dames-damesfietsen/m2064554806-batavus-damesfiets-26-inch",
-                }
-            ],
-        }
+    with requests_mock.Mocker() as m:
+        m.get(
+            "https://www.marktplaats.nl/lrp/api/search",
+            status_code=200,
+            headers={
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
+                "Accept": "application/json",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin",
+            },
+            text=get_mock_file("query_response.json"),
+        )
 
         query = SearchQuery(
             "fiets",
@@ -109,36 +38,28 @@ def test_request() -> None:
             extra_attributes=[1],
         )
 
-        get_request.assert_called_once_with(
-            "https://www.marktplaats.nl/lrp/api/search",
-            params={
-                "attributeRanges[]": [
-                    "PriceCents:1000:20000",
-                ],
-                "attributesByKey[]": [
-                    f"offeredSince:{int(datetime(2024, 12, 31, 14, 10, 0).timestamp() * 1000)}",
-                ],
-                "attributesById[]": [1],
-                "limit": "1",
-                "offset": "0",
-                "query": "fiets",
-                "searchInTitleAndDescription": "true",
-                "viewOptions": "list-view",
-                "distanceMeters": "1000000",  # basically unlimited
-                "postcode": "",
-                "sortBy": "OPTIMIZED",
-                "sortOrder": "INCREASING",
-                "l1CategoryId": "322",
-                "l2CategoryId": "1415",
-            },
-            headers={
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
-                "Accept": "application/json",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-origin",
-            },
-            timeout=15,
-        )
+        assert m.called
+        req = m.request_history[0]
+        assert req.method == "GET"
+        assert req.qs == {
+            "attributeranges[]": ["pricecents:1000:20000"],
+            "attributesbykey[]": [
+                f"offeredsince:{int(datetime(2024, 12, 31, 14, 10, 0).timestamp() * 1000)}"
+            ],
+            "attributesbyid[]": ["1"],
+            "limit": ["1"],
+            "offset": ["0"],
+            "query": ["fiets"],
+            "searchintitleanddescription": ["true"],
+            "viewoptions": ["list-view"],
+            "distancemeters": ["1000000"],
+            "postcode": [""],
+            "sortby": ["optimized"],
+            "sortorder": ["increasing"],
+            "l1categoryid": ["322"],
+            "l2categoryid": ["1415"],
+        }
+        assert req.timeout == 15
 
     listings = query.get_listings()
     assert len(listings) == 1
@@ -156,33 +77,25 @@ def test_request() -> None:
     assert listing.seller.id == 7405065
     assert not listing.seller.is_verified
 
-    with patch("requests.get") as get_request:
-        get_request.return_value.text = """{
-            "bankAccount": true,
-            "phoneNumber": true,
-            "identification": false,
-            "paymentMethod": {
-                "name": "ideal"
-            },
-            "numberOfReviews": 26,
-            "averageScore": 5,
-            "smbVerified": false,
-            "profilePictures": {}
-        }"""
-
-        seller = listing.seller.get_seller()
-
-        get_request.assert_called_once_with(
+    with requests_mock.Mocker() as m:
+        m.get(
             "https://www.marktplaats.nl/v/api/seller-profile/7405065",
-            params=None,
+            status_code=200,
             headers={
                 "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
                 "Accept": "application/json",
                 "Sec-Fetch-Mode": "cors",
                 "Sec-Fetch-Site": "same-origin",
             },
-            timeout=15,
+            text=get_mock_file("seller_response.json"),
         )
+
+        seller = listing.seller.get_seller()
+
+        req = m.request_history[0]
+        assert req.method == "GET"
+        assert req.qs == {}
+        assert req.timeout == 15
 
     assert not seller.is_verified  # should still be false
     assert seller.id == 7405065  # should still be the same
