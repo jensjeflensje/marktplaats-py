02deaeb249091fe0cc7aac7bee81d77da3480f25
qadzek <84473512+qadzek@users.noreply.github.com>
2026-05-29 19:34:56 +0200

    test: add 2dehands coverage

--- tests/test_images.py
+++ tests/test_images.py
@@ -21,3 +21,16 @@ def test_parse_images() -> None:
     urls = fetch_listing_images("m123456789", Platform.MARKTPLAATS)
     assert len(urls) == 9
     assert urls[0].startswith("https://images.marktplaats.com")
+
+
+@responses.activate
+def test_parse_images_2dehands() -> None:
+    responses.get(
+        "https://link.2dehands.be/m2404214127",
+        status=200,
+        body=get_mock_file("image_response_2dehands.html"),
+    )
+
+    urls = fetch_listing_images("m2404214127", Platform.TWEEDEHANDS)
+    assert len(urls) == 3
+    assert urls[0].startswith("https://images.2dehands.com")
